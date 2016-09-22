9da9df6f46ede5b2be2a1f6cef776beecd44f5fe
Rodion222 <borisevich222@gmail.com>
2016-09-22 13:33:18 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- matrix() #begins by setting the empty matrix as a placeholder for a 
+  #future value
+  
+  setmatrix <- function(y) { 
+    x <<- y
+    inv <<- matrix()
+  } #defines a function to set the matrix, x, to a new matrix, y, and resets 
+  #the inverse matrix, m to empty one  
+  
+  
+  getmatrix <- function() x #returns the matrix x
+  setinverse <- function(inverse) inv <<- inverse #sets the inverse, m, to mean
+  
+  
+  getinverse <- function() inv #returns the mean, inv
+  
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse) 
+  
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# by the task of the assesment it is assumed that 
+#a matrix used is reversable (and square...?)
+cacheSolve <- function(x, ...) {#x here is an object (list) containing 
+  #functions defined above
+  
+  inv <- x$getinverse()
+  if(!is.na(inv[1,1])) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$getmatrix()
+  inv <- solve(data) #calc of the inverse one  
+  x$setinverse(inv)
+  inv
 }
